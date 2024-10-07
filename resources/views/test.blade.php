81c7e90d6c3af9c8db76e5edcfd2435c94368083
jamaela <aicaj915@gmail.com>
2024-10-07 17:25:05 +0800

    make

---
+++ resources/views/test.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=h1, initial-scale=1.0">
+    <titl>test</title>
+</head>
+<body>
+    <h1>Test Routes</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/test', function () {
+    return view('test');
+})->name('test');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
